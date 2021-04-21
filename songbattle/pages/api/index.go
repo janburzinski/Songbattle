164bcbd03f42fb7e0f3192b1de1d874b5de8ac21
Urento <jan@burzinski.de>
2021-04-21 14:33:59 +0200

    pls work

--- songbattle/pages/api/index.go
+++ songbattle/pages/api/index.go
@@ -3,10 +3,8 @@ package handler
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintf(w, currentTime)
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
