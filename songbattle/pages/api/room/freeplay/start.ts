3fa0d81ed56da8f8abc09e396ba9126afcd7b830
Urento <jan@burzinski.de>
2021-05-24 12:09:20 +0200

    optimized sql query when creating freeplay room and added loading indication when creating room

--- songbattle/pages/api/room/freeplay/start.ts
+++ songbattle/pages/api/room/freeplay/start.ts
@@ -1,6 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { connectToDb } from "../../../../utils/connectToDb";
 import { generateId } from "../../../../utils/consts";
+import format from "pg-format";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   if (req.method.toLowerCase() === "post") {
@@ -17,20 +18,76 @@ export default async (req: VercelRequest, res: VercelResponse) => {
     }
     const db = await connectToDb();
     const songs = [
-      "https://open.spotify.com/track/2IePEfMAtqWS6rLXXFZIgI?si=c549e9fe7bab48ea",
-      "https://open.spotify.com/track/1HyZhieUhfrHnI2xhC8Ujz?si=c70d638836324d37",
-      "https://open.spotify.com/track/14mc78ctIxHOMYz5fWja7Q?si=ef49b9a1965046af",
-      "https://open.spotify.com/track/0xAY7oG6h1YBv0Tvc8eUnu?si=755cb05cf9724425",
-      "https://open.spotify.com/track/4zb2Qicw1hoIaQ6Lg7BqjK?si=c9571aee71984461",
-      "https://open.spotify.com/track/0cf6Z2QLqShle0CQQeK9Do?si=f3ad0b72164b40c8",
-      "https://open.spotify.com/track/1D3z6HTiQsNmZxjl7F7eoG?si=b4c56ebd535a4e3c",
-      "https://open.spotify.com/track/40mjsnRjCpycdUw3xhS20g?si=1c036513fba5482d",
-      "https://open.spotify.com/track/4G8SyOXJeXXzi3u9xT9GUx?si=a73546b7f4ab4547",
-      "https://open.spotify.com/track/43PGPuHIlVOc04jrZVh9L6?si=74892a7d6cc8466d",
-      "https://open.spotify.com/track/21UkXrc9kD48rNpTMI2ecz?si=d08fb6088c1441b2",
-      "https://open.spotify.com/track/5kMCBppYo4eaAXlvOBj6Ti?si=ac5f8746a40f404f",
-      "https://open.spotify.com/track/6uvMKqNlrSvcC4NaKnrwjZ?si=ec628b68f4494ce8",
-      "https://open.spotify.com/track/0SiddXSHfp3rXlwLWHi5z6?si=d2faa0acf8fc4a91",
+      [
+        id,
+        "https://open.spotify.com/track/2IePEfMAtqWS6rLXXFZIgI?si=c549e9fe7bab48ea",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/1HyZhieUhfrHnI2xhC8Ujz?si=c70d638836324d37",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/14mc78ctIxHOMYz5fWja7Q?si=ef49b9a1965046af",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/0xAY7oG6h1YBv0Tvc8eUnu?si=755cb05cf9724425",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/4zb2Qicw1hoIaQ6Lg7BqjK?si=c9571aee71984461",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/0cf6Z2QLqShle0CQQeK9Do?si=f3ad0b72164b40c8",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/1D3z6HTiQsNmZxjl7F7eoG?si=b4c56ebd535a4e3c",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/40mjsnRjCpycdUw3xhS20g?si=1c036513fba5482d",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/4G8SyOXJeXXzi3u9xT9GUx?si=a73546b7f4ab4547",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/43PGPuHIlVOc04jrZVh9L6?si=74892a7d6cc8466d",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/21UkXrc9kD48rNpTMI2ecz?si=d08fb6088c1441b2",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/5kMCBppYo4eaAXlvOBj6Ti?si=ac5f8746a40f404f",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/6uvMKqNlrSvcC4NaKnrwjZ?si=ec628b68f4494ce8",
+        owner,
+      ],
+      [
+        id,
+        "https://open.spotify.com/track/0SiddXSHfp3rXlwLWHi5z6?si=d2faa0acf8fc4a91",
+        owner,
+      ],
     ];
     const randomPickedSongs = songs
       .sort(() => 0.5 - Math.random())
@@ -55,12 +112,12 @@ export default async (req: VercelRequest, res: VercelResponse) => {
         return;
       }
       if (r.rowCount >= 1) {
-        for (let i = 0; i < randomPickedSongs.length; i++) {
-          db.query(
-            "INSERT INTO songs(id, songlink, username) VALUES($1,$2,$3)",
-            [id, randomPickedSongs[i], owner]
-          );
-        }
+        const query = format(
+          "INSERT INTO songs(id, songlink, username) VALUES %L",
+          randomPickedSongs
+        );
+        db.query(query);
+
         res.send({ id: id, created: true, secretId: secretId });
         return;
       }
--- songbattle/pages/index.tsx
+++ songbattle/pages/index.tsx
@@ -3,13 +3,20 @@ import { generateId, url } from "../utils/consts";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 import { useCookies } from "react-cookie";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
   const [cookie, setCookie] = useCookies(["user"]);
+  const [loading, setLoading] = useState(false);
+
+  const updateLoadingState = () => {
+    setLoading(!loading);
+  };
 
   const createRoom = async (e) => {
     e.preventDefault();
+    updateLoadingState();
     const username = e.target.username.value;
 
     const res = await fetch(`${url}/api/room/create`, {
@@ -20,6 +27,7 @@ export default function Home() {
       method: "post",
     });
     const result = await res.json();
+    if (result != null) updateLoadingState();
     setCookie("user", result.secretId, {
       path: "/",
       maxAge: 3600, //should be 1 hours or something like that
@@ -30,6 +38,7 @@ export default function Home() {
 
   const startFreeplay = async (e) => {
     e.preventDefault();
+    updateLoadingState();
     const id = generateId();
 
     const res = await fetch(`${url}/api/room/freeplay/start`, {
@@ -43,6 +52,7 @@ export default function Home() {
     });
 
     const result = await res.json();
+    if (result != null) updateLoadingState();
     // Add Cookie for auth
     setCookie("user", result.secretId, {
       path: "/",
@@ -60,61 +70,58 @@ export default function Home() {
       <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
-              Create a Room
-            </h2>
+            {loading ? (
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
+                Creating the Room...
+              </h2>
+            ) : (
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
+                Create a Room
+              </h2>
+            )}
           </div>
-          <form
-            className="mt-8 space-y-6"
-            action="#"
-            onSubmit={createRoom}
-            method="POST"
-          >
-            <div>
-              <label htmlFor="username" className="sr-only">
-                Username
-              </label>
-              <input
-                id="username"
-                name="username"
-                type="text"
-                autoComplete="username"
-                required
-                className="dark:bg-gray-800 dark:text-white appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Username"
-              />
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                CREATE
-              </button>
-              <br />
-              <button
-                onClick={startFreeplay}
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                FREEPLAY
-              </button>
-            </div>
-          </form>
+          {!loading ? (
+            <form
+              className="mt-8 space-y-6"
+              action="#"
+              onSubmit={createRoom}
+              method="POST"
+            >
+              <div>
+                <label htmlFor="username" className="sr-only">
+                  Username
+                </label>
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  className="dark:bg-gray-800 dark:text-white appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Username"
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  CREATE
+                </button>
+                <br />
+                <button
+                  onClick={startFreeplay}
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  FREEPLAY
+                </button>
+              </div>
+            </form>
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
-      <footer className={styles.footer}>
-        <a
-          href="https://github.com/Urento/Songbattle"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/GitHub-Mark-Light-120px-plus.png"
-            alt="Github Logo"
-            className={styles.logo}
-          />
-        </a>
-      </footer>
     </div>
   );
 }
