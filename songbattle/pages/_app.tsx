85141307fa4a33902b6bac0e836eb315b70fddeb
Urento <jan@burzinski.de>
2021-05-30 12:44:47 +0200

    Added NProgress for better user experience

--- songbattle/pages/_app.tsx
+++ songbattle/pages/_app.tsx
@@ -1,9 +1,22 @@
 import { CookiesProvider } from "react-cookie";
+import Router from "next/router";
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
     <CookiesProvider>
+      <Head>
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
       <Component {...pageProps} />
     </CookiesProvider>
   );
